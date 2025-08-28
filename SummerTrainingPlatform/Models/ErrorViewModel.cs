8ea11d382805bdbc38ebcce9fe24d67701575254
Mareem Kareem <mk2146@fayoum.edu.eg>
2025-08-28 17:28:02 +0300

    Add project files.

---
+++ SummerTrainingPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SummerTrainingPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
